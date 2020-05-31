871d5cac6c7a294513c843b926a7800b0ddfb9dc
Jacky Chan <chankongching@gmail.com>
2020-05-31 17:58:15 +0800

    Remove set response

--- web.py
+++ web.py
@@ -47,7 +47,7 @@ class S(BaseHTTPRequestHandler):
         print(str(self.headers))
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
-        self._set_response()
+        # self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 
